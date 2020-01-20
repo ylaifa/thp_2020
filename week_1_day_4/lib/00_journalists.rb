0a36c51c6c4c4229b370fccc1a4d6a7021ecc0ff
Yousri <laifayousri@gmail.com>
2020-01-20 17:50:00 +0100

    exercices finished

---
+++ week_1_day_4/lib/00_journalists.rb
@@ -0,0 +1,164 @@
+handles_twitter = ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+
+# Combien y a-t-il de journalistes dans ce array ?
+
+def how_many_journalists(handles_twitter)
+    return handles_twitter.count
+end
+
+# how_many_journalists(handles_twitter)
+
+# Combien d'handle contiennent un numéro ?
+
+def how_many_journalists_with_number(handles_twitter)
+    counter = 0
+    handles_twitter.each {|handle| counter += 1 if handle =~ /\d/}
+    return counter
+end
+
+# how_many_journalists_with_number(handles_twitter)
+
+# Combien d'handle contiennent les 4 lettres du prénom "Aude" à la suite (sans prendre en compte les majuscules) ?
+
+def how_many_journalists_with_Aude?(handles_twitter)
+    counter = 0
+    handles_twitter.each {|handle| counter += 1 if handle.downcase =~ /(aude)/}
+    return counter
+end
+
+# how_many_journalists_with_Aude?(handles_twitter)
+
+# Combien commencent par une majuscule (première lettre juste après le @) ?
+
+def first_upper?(handles_twitter)
+    counter = 0
+    handles_twitter.each {|handle| counter += 1 if handle[1] =~ /[A-Z]/ }
+    return counter
+end
+
+# first_upper?(handles_twitter)
+
+# Combien contiennent au moins une majuscule ?
+
+def how_many_with_capital(handles_twitter)
+    counter = 0
+    handles_twitter.each { |handle| counter += 1 if handle =~ /[A-Z]/ }
+    return counter
+end
+
+# how_many_with_capital(handles_twitter)
+
+# Combien y a-t-il de underscore _ dans tous les pseudos confondus ?
+
+def how_many_underscore(handles_twitter)
+    counter = 0
+    handles_twitter.each {|handle| counter += handle.count("_")}
+    return counter
+end
+
+# how_many_underscore(handles_twitter)
+
+# Trie la liste de handle par ordre alphabétique.
+
+def alphabetic_sort(handles_twitter)
+    return handles_twitter.sort_by {|handle| handle.downcase } 
+end
+
+# alphabetic_sort(handles_twitter)
+
+# Quels sont les 50 handles les plus courts de ce array ?
+
+def fifty_shortest(handles_twitter)
+    return handles_twitter.sort_by{|s| s.length }[0...50]
+end
+
+# fifty_shortest(handles_twitter)
+
+# Quelle est la position dans l'array de la personne @epenser ?
+
+def find_index_of_element(handles_twitter)
+    return handles_twitter.find_index("@epenser")
+end
+
+# find_index_of_element(handles_twitter)
+
+def menu(handles_twitter)
+    menu = true
+
+    puts "\n\n"
+    puts "=" * 35 + " Jouons avec nos journalistes " +  "=" * 35 + "\n\n"
+    puts "1) Combien de journalistes de BFM TV ?"
+    puts "2) Combien de journalistes on un nombre dans leur pseudo ?"
+    puts "3) Combien de journalistes on 'Aude' dans leur pseudo ?"
+    puts "4) Combien de journalistes ont un pseudo qui commence par une lettre majuscule ?"
+    puts "5) Combien de journalistes ont un pseudo qui contient une lettre majuscule ?"
+    puts "6) Combien d'underscores dans tous les pseudos des journalistes ?"
+    puts "7) Trier les journalistes par ordre alphabétique."
+    puts "8) Afficher les 50 pseudos de journalistes les plus court."
+    puts "9) Quel est le numéro d'index de @epenser ?"
+    puts "\n"
+    puts "10) Quitter"
+    print "> "
+
+    choise_number = gets.chomp
+   
+    case choise_number
+    when '1'
+        puts "\n\n"
+        puts "Il y a #{how_many_journalists(handles_twitter)} sur BFM TV."
+        print "\n[Enter] pour continuer"
+        gets.chomp
+    when '2'
+        puts "\n\n"
+        puts "Il y a #{how_many_journalists_with_number(handles_twitter)} pseudos avec un numéro."
+        print "\n[Enter] pour continuer"
+        gets.chomp
+    when '3'
+        puts "\n\n"
+        puts "Il y a #{how_many_journalists_with_Aude?(handles_twitter)} pseudos inclant \"Aude\"."
+        print "\n[Enter] pour continuer"
+        gets.chomp
+    when '4'
+        puts "\n\n"
+        puts "Il y a #{first_upper?(handles_twitter)} pseudos commencent par une lettre majuscule."
+        print "\n[Enter] pour continuer"
+        gets.chomp
+    when '5'
+        puts "\n\n"
+        puts "Il y a #{how_many_with_capital(handles_twitter)} pseudos qui contiennent une lettre majuscule."
+        print "\n[Enter] pour continuer"
+        gets.chomp
+    when '6'
+        puts "\n\n"
+        puts "Il y a #{how_many_underscore(handles_twitter)} underscore dans les pseudos."
+        print "\n[Enter] pour continuer"
+        gets.chomp
+    when '7'
+        puts "\n\n"
+        puts "Pseudo par ordre alphabétique : "
+        puts alphabetic_sort(handles_twitter)
+        print "\n[Enter] pour continuer"
+        gets.chomp
+    when '8'
+        puts "\n\n"
+        puts "Liste des 50 pseudos les plus court : "
+        puts fifty_shortest(handles_twitter)
+        print "\n[Enter] pour continuer"
+        gets.chomp
+    when '9'
+        puts "\n\n"
+        print "Le pseudo @epenser est en position: "
+        puts find_index_of_element(handles_twitter)
+        print "\n[Enter] pour continuer"
+        gets.chomp
+    when '10'
+        menu = false
+        puts "\n"
+        puts "=" * 35 + " A bientot ! " +  "=" * 35 + "\n\n"
+    end
+
+    menu(handles_twitter) if menu
+    
+end
+
+menu(handles_twitter)
