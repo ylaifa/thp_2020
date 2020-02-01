6128ec405c5f7d23d04a4ace0ee065f3e6d9c601
Yousri <laifayousri@gmail.com>
2020-02-01 21:44:29 +0100

    Edit controller.rb

--- week_3_day_5/the_hacking_gossip_ruby_version_POO/lib/controller.rb
+++ week_3_day_5/the_hacking_gossip_ruby_version_POO/lib/controller.rb
@@ -21,5 +21,5 @@ class Controller
         gossips = Gossip.all
         delete_gossip_choice = @view.delete_gossip(gossips)
         Gossip.delete(gossips, delete_gossip_choice)
-      end
+        end
 end
