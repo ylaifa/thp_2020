b9e5329dd0f3f8182d98c98f9212decc9ef2c5ea
Yousri <laifayousri@gmail.com>
2020-02-09 16:02:14 +0100

    edit seeds.rb

--- week_4_day_4/freedoc/db/seeds.rb
+++ week_4_day_4/freedoc/db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+Doctor.destroy_all
+Patient.destroy_all
+Appointment.destroy_all
+
+10.times do 
+    Doctor.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, speciality: ["General", "Dentist", "ORL"].sample, zip_code: Faker::Alphanumeric.alphanumeric(number: 5, min_alpha: 3))
+    Patient.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
+end
+
+20.times do
+    Appointment.create(date: Faker::Date.between_except(from: 1.year.ago, to: 1.year.from_now, excepted: Date.today), doctor: Doctor.last(10).sample, patient: Patient.last(10).sample)
+end
