29921e365c67000d70c3e460dc5a8e93833c67c4
Yousri <laifayousri@gmail.com>
2020-02-09 19:41:54 +0100

    edit seeds.rb

--- week_4_day_4/freedoc/db/seeds.rb
+++ week_4_day_4/freedoc/db/seeds.rb
@@ -10,12 +10,35 @@ require 'faker'
 Doctor.destroy_all
 Patient.destroy_all
 Appointment.destroy_all
+City.destroy_all
+Speciality.destroy_all
+
+30.times do
+    City.create(name: Faker::Nation.capital_city)
+end
 
 10.times do 
-    Doctor.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, speciality: ["General", "Dentist", "ORL"].sample, zip_code: Faker::Alphanumeric.alphanumeric(number: 5, min_alpha: 3))
-    Patient.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
+    Doctor.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, zip_code: Faker::Alphanumeric.alphanumeric(number: 5, min_alpha: 3), city_id: Faker::Number.between(from: City.last.id - 30, to: City.last.id))
+    Patient.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, city_id: Faker::Number.between(from: City.last.id - 30, to: City.last.id))
+end
+
+specialities = ["Generalist", "ORL", "Dentist", "Neurologist"]
+
+specialities.each do |speciality|
+    Speciality.create(name: speciality)
+
 end
 
 20.times do
-    Appointment.create(date: Faker::Date.between_except(from: 1.year.ago, to: 1.year.from_now, excepted: Date.today), doctor: Doctor.last(10).sample, patient: Patient.last(10).sample)
+    Appointment.create(date: Faker::Date.between_except(from: 1.year.ago, to: 1.year.from_now, excepted: Date.today), doctor: Doctor.last(10).sample, patient: Patient.last(10).sample, city_id: Faker::Number.between(from: City.last.id - 30, to: City.last.id))
 end
+
+doctors = Doctor.all 
+specialities = Speciality.all
+
+i = doctors.length 
+10.times do
+    Doctor.find(i).specialities = specialities.sample(2)
+    i -= 1
+end
+
